280c004dbf20982fcda029a447fb16571371dd22
Dein Name <deine.email@example.com>
2024-08-26 17:48:27 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+let dictionary = JSON.parse(localStorage.getItem("dictionary")) || {};
+let randomGermanWord;
+
+function addVocabulary() {
+    dictionary[germanText.value] = englishText.value;
+
+    germanText.value = '';
+    englishText.value = '';
+
+    localStorage.setItem('dictionary', JSON.stringify(dictionary));
+    render()
+}
+
+function deleteVocabulary(key) {
+    delete dictionary[key];
+    localStorage.setItem('dictionary', JSON.stringify(dictionary));
+    render();
+}
+
+function render() {
+    vocabularyList.innerHTML = '';
+    for (let key in dictionary) {
+        vocabularyList.innerHTML += `<li>${key} - ${dictionary[key]} <button onclick="deleteVocabulary('${key}')">✖</button></li>`;
+    }
+}
+
+function nextVocabulary(){
+    let obj_keys = Object.keys(dictionary);
+    randomGermanWord = obj_keys[Math.floor(Math.random() * obj_keys.length)];
+    word.innerHTML = `${dictionary[randomGermanWord]}?`;
+}
+
+function compare() {
+    if(germanText.value == randomGermanWord) {
+        text.innerHTML = 'Richtig!!';
+    } else {
+        text.innerHTML = 'Falsch!!';
+    }
+    germanText.value = '';
+    nextVocabulary();
+}
